df8673b4e8e41cbd1ad1d0fc3965e6e3a9962e03
SEBASTIAO RODRIGO <rodrigoexer1@gmail.com>
2025-02-21 11:08:52 -0300

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,68 @@
-import './styles/App.css'
-import { ThemeOption } from './components/theme/ThemeOption'
-import kLogo from "/logo.svg"
-import { Link } from 'react-router-dom'
+import "./styles/App.css";
+import { ThemeOption } from "./components/theme/ThemeOption";
+import kLogo from "/logo.svg";
+import { Link } from "react-router-dom";
+import { motion } from "motion/react";
 
 function App() {
+  const selectedTheme = localStorage.getItem("theme");
 
-  const selectedTheme = localStorage.getItem("theme")
-
-  if (selectedTheme){
-    document
-      .querySelector("body")
-      ?.setAttribute("data-theme", selectedTheme)
+  if (selectedTheme) {
+    document.querySelector("body")?.setAttribute("data-theme", selectedTheme);
   }
 
   return (
-    <div className='div-login-form'>
-      
-      <img src={kLogo} alt="" />
+    <div className="div-login-form">
+      <motion.div
+        initial={{ opacity: 0, scale: 0 }}
+        whileInView={{ opacity: 1, scale: 1.1 }}
+        exit={{ opacity: 0, scale: 0 }}
+        transition={{ duration: 1.9 }}
+        className="top-10 text-center"
+      >
+        <img src={kLogo} alt="" />
 
-      <div className="textos">
-        <h1>Entrar na sua conta</h1>
-        <small>Ou <Link  to="/cadastrar">fazer cadastro</Link></small>
-      </div>
-
-      <form className='form-login-entrar'>
-        <div className="theme-options">
-          <h3 className='theme-title'>Tema</h3>
-          <ThemeOption theme='dark' />
-          <ThemeOption theme='light' />
+        <div className="textos">
+          <h1>Entrar na sua conta</h1>
+          <small className="m">
+            Ou <Link to="/cadastrar">fazer cadastro</Link>
+          </small>
         </div>
+      </motion.div>
 
-        <div className="div-input-entrar">
-          <label htmlFor="">E-mail</label>
-          <input type="text" />
-        </div>
+      <motion.div
+        initial={{ opacity: 0, scale: 0 }}
+        whileInView={{ opacity: 1, scale: 1.1 }}
+        exit={{ opacity: 0, scale: 0 }}
+        transition={{ duration: 3 }}
+        className="text-center mt-5"
+      >
+        <form className="form-login-entrar">
+          <div className="theme-options">
+            <h3 className="theme-title">Tema</h3>
+            <ThemeOption theme="dark" />
+            <ThemeOption theme="light" />
+          </div>
 
-        <div className="div-input-entrar">
-          <label htmlFor="">Senha</label>
-          <input type="password" />
-        </div>
+          <div className="div-input-entrar">
+            <label htmlFor="">E-mail</label>
+            <input type="text" />
+          </div>
 
-        <Link className='esqueceu' to='/esqueceuSenha'>Esqueceu a senha ?</Link>
+          <div className="div-input-entrar">
+            <label htmlFor="">Senha</label>
+            <input type="password" />
+          </div>
 
-        <button className='btn-entrar-login'>Entrar</button>
-      </form>
+          <Link className="esqueceu" to="/esqueceuSenha">
+            Esqueceu a senha ?
+          </Link>
+
+          <button className="btn-entrar-login">Entrar</button>
+        </form>
+      </motion.div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
